b301b57b64ddc53df3f943ba8689baa0f592f901
pam <pamelarozolem@gmail.com>
2018-12-18 01:44:24 -0200

    modificando tamplate de registro

--- src/core/templateRegister.js
+++ src/core/templateRegister.js
@@ -3,8 +3,7 @@ const Settings = require('../config/settings');
 class TemplateRegister {
     
     static template(name,hash){
-      console.log('template');
-        return `<div style="font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size:16px;line-height:1.4em;margin:0;padding:0;text-align:center">
+        return `<div style="font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size:16px;line-height:1.0em;margin:0;padding:0;text-align:center">
         <table style="margin:0 auto;"width="400" cellspacing="0" cellpadding="0" border="0">
           <tbody>
             <tr>
@@ -16,7 +15,7 @@ class TemplateRegister {
             </tr>
             <tr>
               <td align="center">
-                <a href="${Settings.appClientConfirm}/${hash}"style="background-color:#f43d4c;border:1px solid #283645;padding:10px;border-radius:10px;color:white;text-decoration:none;cursor:pointer">Confirme</a>
+                <a href="${Settings.appClientConfirm}/${hash}"style="background-color:#f43d4c;border:1px solid #f43d4c;padding:10px;border-radius:10px;color:white;text-decoration:none;cursor:pointer">Confirme</a>
               </td>
             </tr>
           </tbody>
